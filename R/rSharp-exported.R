6419d3f3400854467732646c8fc176134cede0de
PavelBal <pavel.balazki@gmail.com>
2024-02-29 16:34:32 +0100

    Rename rToClrType to rToDotNetType

--- R/rSharp-exported.R
+++ R/rSharp-exported.R
@@ -136,65 +136,6 @@ clrNew <- function(typename, ...) {
   .mkClrObjRef(o, clrtype = typename)
 }
 
-#' Get the type code for a SEXP
-#'
-#' Get the type code for a SEXP, as returned by the TYPEOF macro
-#'
-#' @param sexp an R object
-#' @return the type code, an integer, as defined in Rinternals.h
-#' @export
-getSexpType <- function(sexp) {
-  extPtr <- .External("r_get_sexp_type", sexp, PACKAGE = rSharpEnv$nativePkgName)
-  return(.mkClrObjRef(extPtr))
-}
-
-#' Peek into the structure of R objects 'as seen from C code'
-#'
-#' Inspect one or more R object to get information on its representation in the engine.
-#' This function is mostly useful for R/rSharp developers. It is derived from the 'showArgs'
-#' example in the R extension manual
-#'
-#' @param ... one or more R objects
-#' @return NULL. Information is printed, not returned.
-#' @export
-inspectArgs <- function(...) {
-  extPtr <- .External("r_show_args", ..., PACKAGE = rSharpEnv$nativePkgName)
-  # return(.mkClrObjRef(extPtr))
-}
-
-#' Get the COM variant type of a CLR object
-#'
-#' Get the COM variant type of a CLR object, e.g. "VT_ARRAY | VT_I8". This function only works when run on the Microsoft implementation of the CLR.
-#' This function is useful for advanced diagnosis; most users should never have to use it.
-#'
-#' @param objOrType a CLR object, or type name, possibly namespace and assembly qualified type name, e.g. 'My.Namespace.MyClass,MyAssemblyName'.
-#' @param methodName the name of the method called on the object or class specified with objOrType
-#' @param ... one or more arguments to pass to the function call
-#' @examples
-#' \dontrun{
-#' library(rSharp)
-#' cTypename <- getRSharpSetting("testCasesTypeName")
-#' #         public static bool IsTrue(bool arg)
-#' }
-
-#' Gets the type of a CLR object resulting from converting an R object
-#'
-#' Gets the type of a CLR object resulting from converting an R object. This function is mostly for documentation purposes, but may be of use to end users.
-#'
-#' @param x An R objects
-#' @return A list, with columns including mode, type,class,length and the string of the corresponding CLR type.
-#' @export
-rToClrType <- function(x) {
-  list(
-    # what = str(x),
-    mode = mode(x),
-    type = typeof(x),
-    class = class(x),
-    length = length(x),
-    clrType = callStatic("ClrFacade.ClrFacade", "GetObjectTypeName", x)
-  )
-}
-
 #' Gets the type of a CLR object given its type name
 #'
 #' Gets the type of a CLR object given its type name
--- R/utilities-diagnostics.R
+++ R/utilities-diagnostics.R
@@ -1,5 +1,7 @@
 #' Get the type code for a SEXP
 #'
+#' @description
+#'
 #' Get the type code for a SEXP, as returned by the TYPEOF macro
 #'
 #' @param sexp an R object
@@ -7,60 +9,38 @@
 #' @export
 getSexpType <- function(sexp) {
   extPtr <- .External("r_get_sexp_type", sexp, PACKAGE = rSharpEnv$nativePkgName)
-  return(createNetObject(extPtr))
+  return(.mkClrObjRef(extPtr))
 }
 
 #' Peek into the structure of R objects 'as seen from C code'
 #'
 #' Inspect one or more R object to get information on its representation in the engine.
-#' This function is mostly useful for R/rClr developers. It is derived from the 'showArgs'
+#' This function is mostly useful for R/rSharp developers. It is derived from the 'showArgs'
 #' example in the R extension manual
 #'
 #' @param ... one or more R objects
 #' @return NULL. Information is printed, not returned.
 #' @export
 inspectArgs <- function(...) {
+  #extPtr is NULL
   extPtr <- .External("r_show_args", ..., PACKAGE = rSharpEnv$nativePkgName)
-  # return(createNetObject(extPtr))
 }
 
-#' Get the COM variant type of a CLR object
+#' Gets the type of a .NET object resulting from converting an R object
 #'
-#' Get the COM variant type of a CLR object, e.g. "VT_ARRAY | VT_I8". This function only works when run on the Microsoft implementation of the CLR.
-#' This function is useful for advanced diagnosis; most users should never have to use it.
+#' @description
+#' Gets the type of a .NET object resulting from converting an R object. This function is mostly for documentation purposes, but may be of use to end users.
 #'
-#' @param objOrType a CLR object, or type name, possibly namespace and assembly qualified type name, e.g. 'My.Namespace.MyClass,MyAssemblyName'.
-#' @param methodName the name of the method called on the object or class specified with objOrType
-#' @param ... one or more arguments to pass to the function call
-#' @examples
-#' \dontrun{
-#' cTypename <- getRSharpSetting("testCasesTypeName")
-#' #         public static bool IsTrue(bool arg)
-#' clrVT(cTypename, "IsTrue", TRUE)
-#' clrVT("System.Convert", "ToInt64", 123L)
-#' clrVT("System.Convert", "ToUInt64", 123L)
-#' }
-#' @return A string
+#' @param x An R object
+#' @return A list, with columns including mode, type, class, length and the string of the corresponding .NET type.
 #' @export
-clrVT <- function(objOrType, methodName, ...) {
-  return(callStatic("Rclr.DataConversionHelper", "GetReturnedVariantTypename", objOrType, methodName, ...))
-  # return(createNetObject(extPtr))
-}
-
-#' Gets the type of a CLR object resulting from converting an R object
-#'
-#' Gets the type of a CLR object resulting from converting an R object. This function is mostly for documentation purposes, but may be of use to end users.
-#'
-#' @param x An R objects
-#' @return A list, with columns including mode, type,class,length and the string of the corresponding CLR type.
-#' @export
-rToClrType <- function(x) {
+rToDotNetType <- function(x) {
   list(
     # what = str(x),
     mode = mode(x),
     type = typeof(x),
     class = class(x),
     length = length(x),
-    clrType = callStatic("Rclr.ClrFacade", "GetObjectTypeName", x)
+    clrType = callStatic(rSharpEnv$clrFacadeTypeName, "GetObjectTypeName", x)
   )
 }
