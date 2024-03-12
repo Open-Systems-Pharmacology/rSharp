891a9d5365aeaad17c290432d1437a2bddb26cfc
PavelBal <pavel.balazki@gmail.com>
2024-03-12 13:22:31 +0100

    More tests

--- tests/testthat/test-null-na-nan.R
+++ tests/testthat/test-null-na-nan.R
@@ -1,43 +1,41 @@
 ########## R to .NET tests##########
+test_that("Basic NULL is passed to .NET as null", {
+  expect_true(callTestCase("IsNull", NULL))
+})
+test_that("Basic NA is passed to .NET as NA", {
+  expect_true(callTestCase("IsNA", NA))
+})
+test_that("Basic NaN is passed to .NET as NaN", {
+  expect_true(callTestCase("IsNaN", NaN))
+})
 
-# test_that("Array NULL is passed to .NET as null", {
-#   expect_true(callTestCase("IsNull", c(1, NULL, 3)))
-# })
-# test_that("Array NA is passed to .NET as NA", {
-#   expect_true(callTestCase("IsNull", c(1, NA, 3)))
-# })
-# test_that("Array NaN is passed to .NET as NaN", {
-#   expect_true(callTestCase("IsNull", c(1, NaN, 3)))
-# })
+test_that("NULL in an array is passed to .NET as NULL", {
+  expect_true(callTestCase("IsNull", c(1, NULL, 3)))
+})
+test_that("NA in an array is passed to .NET as NA", {
+  expect_true(callTestCase("IsNA", c(1, NA, 3)))
+})
+test_that("NaN in an array is passed to .NET as NaN", {
+  expect_true(callTestCase("IsNaN", c(1, NaN, 3)))
+})
 
 ########## .NET to R tests##########
-# test_that("Array NULL is passed from .NET as null", {
-#   expect_equal(callTestCase("IsNull", NULL))
-# })
-# test_that("Array NA is passed from .NET as NA", {
-#   expect_equal(callTestCase("IsNull", NA))
-# })
-# test_that("Array NaN is passed from .NET as NaN", {
-#   expect_equal(callTestCase("IsNull", NA))
-# })
+test_that("Basic NULL is passed from .NET as null", {
+  expect_true(callTestCase("GetNull", NULL))
+})
+test_that("Basic NA is passed from .NET as NA", {
+  expect_true(callTestCase("GetNA", NA))
+})
+test_that("Basic NaN is passed from .NET as NaN", {
+  expect_true(callTestCase("GetNaN", NaN))
+})
 
-# https://github.com/Open-Systems-Pharmacology/rSharp/issues/57
-# test_that("Basic NULL is passed to .NET as null", {
-#   expect_true(callTestCase("IsNull", NULL))
-# })
-# test_that("Basic NA is passed to .NET as NA", {
-#   expect_true(callTestCase("IsNull", NA))
-# })
-# test_that("Basic NaN is passed to .NET as NaN", {
-#   expect_true(callTestCase("IsNull", NaN))
-# })
-#
-# test_that("Basic NULL is passed from .NET as null", {
-#   expect_true(callTestCase("IsNull", NULL))
-# })
-# test_that("Basic NA is passed from .NET as NA", {
-#   expect_true(callTestCase("IsNull", NA))
-# })
-# test_that("Basic NaN is passed from .NET as NaN", {
-#   expect_true(callTestCase("IsNull", NaN))
-# })
+test_that("Array NULL is passed from .NET as null", {
+  expect_equal(callTestCase("GetNULLArray", c(1, NULL, 3)))
+})
+test_that("Array NA is passed from .NET as NA", {
+  expect_equal(callTestCase("GetNAArray", c(1, NA, 3)))
+})
+test_that("Array NaN is passed from .NET as NaN", {
+  expect_equal(callTestCase("GetNaNlArray", c(1, NaN, 3)))
+})
