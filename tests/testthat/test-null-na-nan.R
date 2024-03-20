afcdad894abdd875b9385fb32e34e90dc7c624d5
PavelBal <pavel.balazki@gmail.com>
2024-03-20 11:25:35 +0100

    Add to knowledge base information about how to pass NULL in arrays Some tests refactor

--- tests/testthat/test-null-na-nan.R
+++ tests/testthat/test-null-na-nan.R
@@ -2,6 +2,8 @@
 test_that("Basic NULL is passed to .NET as null", {
   expect_true(callTestCase("IsNull", NULL))
 })
+
+# What arrives at .NET side is TRUE - questionable. See https://github.com/Open-Systems-Pharmacology/rSharp/issues/110
 test_that("Basic NA is passed to .NET as NA", {
   expect_true(callTestCase("IsNA", NA))
 })
@@ -10,32 +12,41 @@ test_that("Basic NaN is passed to .NET as NaN", {
 })
 
 test_that("NULL in an array is passed to .NET as NULL", {
-  expect_true(callTestCase("IsNull", c(1, NULL, 3)))
+  expect_true(callTestCase("IsNullInArray", list(1, NULL, 3)))
 })
+
+# What arrives at .NET side is TRUE - questionable. See https://github.com/Open-Systems-Pharmacology/rSharp/issues/110
+# Currently NA is interpreted as Na? Why?
 test_that("NA in an array is passed to .NET as NA", {
-  expect_true(callTestCase("IsNA", c(1, NA, 3)))
+  expect_true(callTestCase("IsNAInArray", c(1, NA, 3)))
 })
 test_that("NaN in an array is passed to .NET as NaN", {
-  expect_true(callTestCase("IsNaN", c(1, NaN, 3)))
+  expect_true(callTestCase("IsNaNInArray", c(1, NaN, 3)))
 })
 
 ########## .NET to R tests##########
 test_that("Basic NULL is passed from .NET as null", {
-  expect_true(callTestCase("GetNull", NULL))
+  expect_null(callTestCase("GetNull"))
 })
-test_that("Basic NA is passed from .NET as NA", {
-  expect_true(callTestCase("GetNA", NA))
-})
+
+# No such concept in .NET
+#https://github.com/Open-Systems-Pharmacology/rSharp/issues/110
+# test_that("Basic NA is passed from .NET as NA", {
+#   expect_true(callTestCase("GetNA", NA))
+# })
+
 test_that("Basic NaN is passed from .NET as NaN", {
-  expect_true(callTestCase("GetNaN", NaN))
+  expect_equal(callTestCase("GetNaN"), NaN)
 })
 
 test_that("Array NULL is passed from .NET as null", {
   expect_equal(callTestCase("GetNULLArray", c(1, NULL, 3)))
 })
-test_that("Array NA is passed from .NET as NA", {
-  expect_equal(callTestCase("GetNAArray", c(1, NA, 3)))
-})
+# No such concept in .NET
+#https://github.com/Open-Systems-Pharmacology/rSharp/issues/110
+# test_that("Array NA is passed from .NET as NA", {
+#   expect_equal(callTestCase("GetNAArray", c(1, NA, 3)))
+# })
 test_that("Array NaN is passed from .NET as NaN", {
   expect_equal(callTestCase("GetNaNlArray", c(1, NaN, 3)))
 })
